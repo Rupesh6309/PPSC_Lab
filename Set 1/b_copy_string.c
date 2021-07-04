918dec1ffa2b3ac11e2c5a7b0d8de0cd841bb3c2
vijaymanikantareddy <83952736+vijaymanikantareddy@users.noreply.github.com>
2021-07-04 17:41:53 +0530

    Create b_copy_string.c

---
+++ Set 1/b_copy_string.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+	char str1[20],str2[20];
+	printf("enter any string: ");
+	scanf("%[^\n]",str1);
+	strcpy(str2,str1);
+	printf("\nafter copying the context to str2 is : %s",str2);
+}
